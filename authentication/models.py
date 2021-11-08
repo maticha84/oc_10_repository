5b98fa5c96cacc79806f7018a8dd6452ce26a320
maticha84 <maticha84@gmail.com>
2021-11-08 11:54:25 +0100

    creation User model in authentication/models.py

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(verbose_name='username', max_length=100, unique=True)
+    first_name = models.CharField(max_length=50, blank=False, verbose_name='Prénom')
+    last_name = models.CharField(max_length=50, blank=False, verbose_name='Nom')
+    email = models.EmailField(unique=True, verbose_name='E-mail')
+
+    USERNAME_FIELDS = ('username',)
+
+    def __str__(self):
+        return self.email
+
+
